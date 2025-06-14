1a1a52dd8da73a5ad5c527ceb28891c38a680dc5
tomaszmol <tmollegro@gmail.com>
2025-06-14 13:14:18 +0200

    Adds FundraisingEvent entity and repository
    Defines the FundraisingEvent entity with relevant fields and JPA annotations.
    Creates a repository interface for managing FundraisingEvent entities in the database.

--- backend/src/main/java/com/charitybox/model/FundraisingEvent.java
+++ backend/src/main/java/com/charitybox/model/FundraisingEvent.java
@@ -1,13 +1,63 @@
 package com.charitybox.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class FundraisingEvent {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "fundraisingEvent")
+    private Set<CollectionBox> collectionBoxes = new HashSet<>();
+
+    @Column(nullable = false)
+    private BigDecimal accountBalance = BigDecimal.ZERO;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Currency accountCurrency = Currency.PLN;
+
+    public FundraisingEvent(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<CollectionBox> getCollectionBoxes() {
+        return collectionBoxes;
+    }
+
+    public void setCollectionBoxes(Set<CollectionBox> collectionBoxes) {
+        this.collectionBoxes = collectionBoxes;
+    }
+
+    public BigDecimal getAccountBalance() {
+        return accountBalance;
+    }
+
+    public void setAccountBalance(BigDecimal accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+
+    public Currency getAccountCurrency() {
+        return accountCurrency;
+    }
+
+    public void setAccountCurrency(Currency accountCurrency) {
+        this.accountCurrency = accountCurrency;
+    }
 }
---
+++ backend/src/main/java/com/charitybox/repository/FundraisingEventRepository.java
@@ -0,0 +1,7 @@
+package com.charitybox.repository;
+
+import com.charitybox.model.FundraisingEvent;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FundraisingEventRepository extends JpaRepository<FundraisingEvent, Long> {
+}
